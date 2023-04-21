9c6bdb44deb40dc880602ddce76cf25beca1449d
6040H0K <ddogonov3@gmail.com>
2023-04-21 19:55:54 +0300

    Add endpoints for formField

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const pg = require('pg')
 const cors = require('cors')
-const routesBotSettings = require('./routes/botSetting.js')
+const routesBotSetting = require('./routes/botSetting.js')
 const routesLanguageProfile = require('./routes/languageProfile.js')
+const routesFormField = require('./routes/formField.js')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/botSettings', routesBotSettings)
+app.use('/api/botSetting', routesBotSetting)
 app.use('/api/languageProfile', routesLanguageProfile)
+app.use('/api/formField', routesFormField)
 
 app.listen(3000, () => {
     console.log(`Server Started at ${3000}`)
---
+++ routes/formField.js
@@ -0,0 +1,57 @@
+const formField = require("../db/formField.js")
+const languageProfile = require("../db/languageProfile.js")
+const express = require("express")
+const router = express.Router()
+
+module.exports = router
+
+router.post("/create", async (req, res) => {
+  let field = await formField.build({
+    question: req.body.question,
+    isOptional: req.body.isOptional,
+    nameField: req.body.nameField,
+    languageProfileId: req.body.languageProfileId,
+  })
+  field.save()
+  res.send(200)
+})
+
+router.get("/all/:languageId", async (req, res) => {
+  let language = await languageProfile.findByPk(req.params.languageId, {
+    include: formField,
+  })
+  let result = []
+  language.formFields.forEach((field) => {
+    result.push(field)
+  })
+  res.send(JSON.stringify(result, null, 2))
+})
+
+router.post("/clearcreate", async (req, res) => {
+  let field = await formField.build({
+    question: "",
+    isOptional: false,
+    nameField: "",
+    languageProfileId: req.body.languageProfileId,
+  })
+  field.save()
+  res.send(200)
+})
+
+router.put("/update", async (req, res) => {
+  let fields = await formField.findAll()
+  for (let i = 0; i < fields.length; i++) {
+    fields[i].question = req.body[`question${fields[i].id}`]
+    fields[i].nameField = req.body[`nameField${fields[i].id}`]
+    fields[i].isOptional = req.body[`isOptional${fields[i].id}`]
+    fields[i].type = req.body[`type${fields[i].id}`]
+    fields[i].save()
+  }
+  res.send(200)
+})
+
+router.delete("/delete/:id", async function (req, res) {
+  let field = await formField.findByPk(req.params.id)
+  field.destroy()
+  res.send(200)
+})
