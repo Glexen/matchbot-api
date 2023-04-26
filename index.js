c222d00301cf5fd2da14349d391ae5341b67e208
6040H0K <ddogonov3@gmail.com>
2023-04-27 01:03:39 +0300

    Add version to endpoints url

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const routesBotSetting = require('./routes/botSetting.js')
 const routesLanguageProfile = require('./routes/languageProfile.js')
 const routesFormField = require('./routes/formField.js')
 
+const version = 'v1'
+
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/botSetting', routesBotSetting)
-app.use('/api/languageProfile', routesLanguageProfile)
-app.use('/api/formField', routesFormField)
+app.use(`/api/${version}/botSetting`, routesBotSetting)
+app.use(`/api/${version}/languageProfile`, routesLanguageProfile)
+app.use(`/api/${version}/formField`, routesFormField)
 
 app.listen(3000, () => {
     console.log(`Server Started at ${3000}`)
