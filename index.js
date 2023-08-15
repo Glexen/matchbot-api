fa9460249595d56b0e1e4e774b802c489669a25b
Dmytro Dohonov <78932896+6040H0K@users.noreply.github.com>
2023-08-15 21:32:28 +0300

    Feature/tb 41 42 43 form constructor (#2)
    * Create structure of project & Add BotSettings and languageProfile tabels in DB & Create endpoints for get adn update data in botSettings table
    
    * Add endpoints for languageProfile
    
    * Add CORS
    
    * Resolve problems from pull request
    
    * Create model in db of formField
    
    * Add endpoints for formField
    
    * BugFix in botSetting.js
    
    * Create endpoint for update one button name, remove all semicolons
    
    * Add endpoints for update hello message, and one emojie
    
    * Add version to endpoints url
    
    * Get botsetting pk frow env
    
    * Config eslint, change endpoints
    
    * Bugfix of naming endpoints
    
    * Remove semicolons

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const express = require('express');
-const pg = require('pg');
-const cors = require('cors');
-const routesBotSettings = require('./routes/botSettings.js');
-const routesLanguageProfile = require('./routes/languageProfile.js');
+const express = require("express")
+const pg = require("pg")
+const cors = require("cors")
+const routesBotSetting = require("./routes/botSetting.js")
+const routesLanguageProfile = require("./routes/languageProfile.js")
+const routesFormField = require("./routes/formField.js")
+const expressListEndpoints = require("express-list-endpoints")
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const version = "v1"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.use(`/api/${version}/botSetting`, routesBotSetting)
+app.use(`/api/${version}/languageProfile`, routesLanguageProfile)
+app.use(`/api/${version}/formField`, routesFormField)
 
 app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
+  console.log(`Server Started at ${3000}`)
+  const routes = expressListEndpoints(app)
+  console.log(routes)
 })
-
-app.use('/api/botSettings', routesBotSettings)
-app.use('/api/languageProfile', routesLanguageProfile)
---
+++ routes/botSetting.js
@@ -0,0 +1,52 @@
+const express = require("express")
+const botSetting = require("../db/botSetting.js")
+const router = express.Router()
+require("dotenv").config()
+
+const botSettingPk = process.env.BOTSETTING_PK
+
+
+module.exports = router
+
+
+// update data endpoint
+router.put("", async (req, res) => {
+  let setting = await botSetting.findByPk(botSettingPk)
+  setting.botToken = req.body.botToken
+  setting.emojies = {
+    like: req.body.like,
+    dislike: req.body.dislike,
+    message: req.body.message,
+    report: req.body.report,
+  }
+  await setting.save()
+  res.send(200)
+})
+
+router.put("/emojies/:emojieName", async (req, res) => {
+  const setting = await botSetting.findByPk(botSettingPk)
+  const emojies = JSON.parse(JSON.stringify(setting.emojies))
+  emojies[req.params.emojieName] = req.body[req.params.emojieName]
+  setting.emojies = emojies
+  await setting.save()
+  res.send(200)
+})
+
+router.put("/botToken", async (req, res) => {
+  const setting = await botSetting.findByPk(botSettingPk)
+  setting.botToken = req.body.botToken
+  await setting.save()
+  res.send(200)
+})
+
+// get data endpoint
+router.get("", async (req, res) => {
+  const setting = await botSetting.findByPk(botSettingPk)
+  res.send(JSON.stringify(setting, null, 2))
+})
+
+router.get("/botToken", async (req, res) => {
+  const setting = await botSetting.findByPk(botSettingPk)
+  console.log(1)
+  res.send(JSON.stringify(setting.botToken))
+})
